b58e745903f7d18ec0e41569cb393d67918b045d
Nikolas Daynard <Nkdaynard@gmail.com>
2024-01-09 19:18:08 -0500

    added files

---
+++ src/main/java/frc/robot/Dashboard.java
@@ -0,0 +1,7 @@
+package frc.robot;
+//just a stub for now
+public class Dashboard {
+    public static <T> void printValue(T value) {
+        System.out.println("The value is: " + value);
+    }
+}
