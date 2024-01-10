958dfb04d97abf081df0919722d384ea18e67e96
Nikolas Daynard <Nkdaynard@gmail.com>
2024-01-09 22:33:42 -0500

    Clarified types in LimelightTable

--- src/main/java/frc/Networking/LimelightTable.java
+++ src/main/java/frc/Networking/LimelightTable.java
@@ -1,7 +1,8 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-
+// more info on limelight networking here 
+// https://docs.limelightvision.io/docs/docs-limelight/apis/complete-networktables-api
 package frc.Networking; // changed from network
 
 import edu.wpi.first.networktables.NetworkTable;
@@ -11,14 +12,17 @@ public class LimelightTable {
 
   private static String TableName = "limelight";
 
-  private static String Tx_Entry = "tx";
-  private static Double Tx_Default = 0.0;
+  private static String X_Offset_Entry = "tx";
+  private static Double X_Offset_Default = 0.0;
   
-  private static String Ty_Entry = "ty";
-  private static Double Ty_Default = 0.0
-  ;
-  private static String Ta_Entry = "ta";
-  private static Double Ta_Default = 0.0;
+  private static String Y_Offset_Entry = "ty";
+  private static Double Y_Offset_Default = 0.0;
+
+  private static String Target_Area_Entry = "ta";
+  private static Double Target_Area_Default = 0.0;
+
+  private static String CurrentTag = "tid";
+  private static Double CurrentTag_Default = 0.0;
 
   private static String CamMode_Entry = "camMode";
   private static Double CamMode_Default = 0.0;
@@ -49,29 +53,33 @@ public class LimelightTable {
   }
 
   public double getTx() {
-    return m_nt.getEntry(Tx_Entry).getDouble(Tx_Default);
+    return m_nt.getEntry(X_Offset_Entry).getDouble(X_Offset_Default);
   }
-  
+
   public void setTx(double value) {
-    m_nt.getEntry(Tx_Entry).setNumber(value);
+    m_nt.getEntry(X_Offset_Entry).setNumber(value);
   }
   
   public double getTy() {
-    return m_nt.getEntry(Ty_Entry).getDouble(Ty_Default);
+    return m_nt.getEntry(Y_Offset_Entry).getDouble(Y_Offset_Default);
   }
-  
+
   public void setTy(double value) {
-    m_nt.getEntry(Ty_Entry).setNumber(value);
+    m_nt.getEntry(Y_Offset_Entry).setNumber(value);
   }
   
   public double getTa() {
-    return m_nt.getEntry(Ta_Entry).getDouble(Ta_Default);
+    return m_nt.getEntry(Target_Area_Entry).getDouble(Target_Area_Default);
   }
-  
+
   public void setTa(double value) {
-    m_nt.getEntry(Ta_Entry).setNumber(value);
+    m_nt.getEntry(Target_Area_Entry).setNumber(value);
   }
-  
+
+  public double getCurrentApriltagId() {
+    return m_nt.getEntry(CurrentTag).getDouble(CurrentTag_Default);
+  }
+
   public double getCamMode() {
     return m_nt.getEntry(CamMode_Entry).getDouble(CamMode_Default);
   }
@@ -95,5 +103,4 @@ public class LimelightTable {
   public void setPipeline(double value) {
     m_nt.getEntry(Pipeline_Entry).setNumber(value);
   }
-
 }
